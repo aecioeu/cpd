d3db2da3d40deeb58a24db36cc3abd42bda93dd3
aeroimports <aeroimports2016@gmail.com>
2024-07-03 10:20:42 -0300

    update

--- public/js/components/whastsapp.js
+++ public/js/components/whastsapp.js
@@ -23,31 +23,49 @@ const sendMessage = async (data) => {
     let respData = await response.json(); //extract JSON from the http response
     console.log(data);
   };
-  
-  const checkWhatsapp = async (number) => {
+  const checkWhatsapp = async (phoneNumber) => {
+    // Atualiza o ícone do grupo de entrada para "🗙" indicando que a verificação está em andamento
     $(".input-group-text").text("🗙");
-    $("#whatsapp").val()
-    const response = await fetch("/api/whatsapp/check", {
-      method: "POST",
-      body: JSON.stringify({ number: number }), // string or object
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    let respData = await response.json(); //extract JSON from the http response
-
-   
-  if(respData){
   
-    if (respData.status == true) {
-      $(".input-group-text").text("✔️");
-      $("#whatsapp").val(respData.jid)
-    } else {
+    // Limpa o campo de entrada do WhatsApp
+    $("#whatsapp").val("");
+  
+    try {
+      // Faz uma requisição POST para a API de verificação do WhatsApp
+      const response = await fetch("/api/whatsapp/check", {
+        method: "POST",
+        body: JSON.stringify({ number: phoneNumber }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+  
+      // Extrai o JSON da resposta HTTP
+      const responseData = await response.json();
+  
+      // Log para depuração
+      console.log(responseData);
+  
+      if (responseData) {
+        if (responseData.exists) {
+          // Atualiza o ícone do grupo de entrada para "✔️" se o número existir no WhatsApp
+          $(".input-group-text").text("✔️");
+          // Preenche o campo de entrada com o JID do WhatsApp
+          $("#whatsapp").val(responseData.jid);
+        } else {
+          // Atualiza o ícone do grupo de entrada para "🗙" se o número não existir no WhatsApp
+          $(".input-group-text").text("🗙");
+          // Limpa o campo de entrada do WhatsApp
+          $("#whatsapp").val("");
+        }
+      }
+    } catch (error) {
+      console.error("Ocorreu um erro durante a verificação do WhatsApp:", error.message);
+      // Em caso de erro, garante que o ícone do grupo de entrada seja "🗙"
       $(".input-group-text").text("🗙");
-      $("#whatsapp").val()
     }
   };
-}
+  
   
   export {
     sendButtonMessage,
